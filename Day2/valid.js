2e88929b4b091ef3b196a206183eec7196c82ac1
Ritalexi <okoliogochukwu83@gmail.com>
2022-04-06 20:00:16 +0100

    second commit

--- Day2/valid.js
+++ Day2/valid.js
@@ -1,34 +1,24 @@
 let result = [];
-function fizzbuzz(num)
+function fizzbuzz ( num )
 {
-    
+   
 for (let i = 1; i <= num; i++)
 {
-    if(i % 3 === 0 && i % 5 === 0)
-
-    {
-       
-        result.push("FIZZBUZZ");
+    if(i % 3 === 0 && i % 5 === 0) {
+        result.push( 'FIZZBUZZ' );
     }
-    if(i % 3 === 0)
-    {
+    if(i % 3 === 0) {
 
-        result.push("FIZZ");
+        result.push( 'FIZZ' );
     }
-    if(i % 5 === 0)
-
+    if( i % 5 === 0 ) {
+        result.push( 'BUZZ' );
+    } else
     {
-
-        result.push( "BUZZ");
-    }
-    
-    else
-    {
-        result.push(i);
+        result.push( i );
     }
 }
 }
+fizzbuzz ( 20 );
 
-fizzbuzz(20);
-
-console.log(result);
+console.log( result );
