d54b45f9f0d8c5e30f118e0b87768393de1ddde0
Ritalexi <okoliogochukwu83@gmail.com>
2022-04-08 17:30:03 +0100

    first commit

---
+++ Day4/day4.js
@@ -0,0 +1,21 @@
+
+function showMyDetails(){
+    const arr = [{
+        name: 'Rita',
+        age: 20,
+        level: '300Level'
+            },{
+        name: 'Okoli',
+        age: 24,
+        level: '500Level'
+            }];
+    const myArr = [];
+    arr.forEach(
+        details => {
+const myDetails = details.name + '/' + details.age + '/' + details.level;
+myArr.push(myDetails);
+        });
+        console.log(myArr);
+    
+}
+console.log(showMyDetails());
