66a132d9cc49b1b6bf84b7df9670f5b90bdc03b2
Ritalexia <70811710+Ritalexi@users.noreply.github.com>
2022-04-08 17:23:55 +0100

    Update day3.js

--- Day3/day3.js
+++ Day3/day3.js
@@ -1,13 +1,9 @@
 let value = [];
 function fizzbuzz (num){
-
- 
-       num.forEach(element => {
+ num.forEach(element => {
            let answer = [];
-        for (let j = 1; j <= element; j++)
-        {
-
-            if(j % 3 === 0 && j % 5 === 0) {
+        for (let j = 1; j <= element; j++) {
+         if(j % 3 === 0 && j % 5 === 0) {
                 let x = 'FIZZBUZZ';
             answer.push(x);
             }
@@ -24,11 +20,9 @@ function fizzbuzz (num){
             let w =  j ;
            answer.push(w);
             }
-        
         }
 value.push(answer);
        });
-   
    }
    fizzbuzz ([10,12,13]);
 console.log(value);
