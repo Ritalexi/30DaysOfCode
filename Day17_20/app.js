75278c44bbde57dfe54c1e0874f05f12e101e31d
Ritalexia <70811710+Ritalexi@users.noreply.github.com>
2022-04-26 10:43:13 +0100

    Update app.js

--- Day17_20/app.js
+++ Day17_20/app.js
@@ -2,12 +2,8 @@ const quote = document.getElementById("advice");
 const reloadButton = document.querySelector('#button');
 const id = document.querySelector('#id');
 
-
 const apiUrl ='https://api.adviceslip.com/advice'
  
-
-
-
 function getQuotes(){
     fetch(apiUrl)
     .then( (resp) => resp.json())
@@ -16,7 +12,6 @@ function getQuotes(){
        quote.innerHTML = '"' + data.slip.advice + '."'
        id.innerHTML = data.slip.id
    })
-
 }
 
 reloadButton.addEventListener('click', () => {
